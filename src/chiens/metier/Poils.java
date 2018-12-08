4349e8771a28d92e582eeebd9e9d14c26ba87370
MKazakova <45536947+MKazakova@users.noreply.github.com>
2018-12-08 20:23:42 +0100

    Update Poils.java

--- src/chiens/metier/Poils.java
+++ src/chiens/metier/Poils.java
@@ -6,10 +6,10 @@ import chiens.bases_donnees.DBTypePoils;
 import chiens.beans.TypePoilsBean;
 
 public class Poils {
-	String description_type_poils;
+    String description_type_poils;
     String message;
 
-    /* traite la création d'un nouveau type de poils */
+    /* traite la crÃ©ation d'un nouveau type de poils */
     public TypePoilsBean creerTypePoils(HttpServletRequest request) throws Exception {
     	request.setCharacterEncoding("UTF-8");
        	TypePoilsBean newTypePoils = null;
@@ -17,7 +17,7 @@ public class Poils {
     	
     	/* on verifie que la description de type de poils n'est pas vide */
     	if(description_type_poils==null||description_type_poils.isEmpty()) {
-    		message="La description de type de poils de chien ne doit pas être vide";	
+    		message="La description de type de poils de chien ne doit pas Ãªtre vide";	
     	}
     	/* on verifie que le nom de poils ne contient pas de chiffres et de longueur correcte */
     	else if(!verifNomTypePoils(description_type_poils)){
@@ -27,13 +27,13 @@ public class Poils {
     		DBTypePoils dbtp = new DBTypePoils();
     		/* on verifie que ce type de poils n'existe pas encore dans la base */
     		if(dbtp.nomExiste(description_type_poils)) {
-    			message=description_type_poils+" existe déjà dans la base";
+    			message=description_type_poils+" existe dÃ©jÃ  dans la base";
     		}
     		else{
     			newTypePoils = new TypePoilsBean();
     			
     			/* si tout se passe bien on initialise le champ description_type_poils 
-    			 * dans TypePoilsBean, ajoute type poils dans la base et recoupère le code généré */
+    			 * dans TypePoilsBean, ajoute type poils dans la base et recoupÃ¨re le code gÃ©nÃ©rÃ© */
     			newTypePoils.setDescription_type_poils(description_type_poils);
     			newTypePoils.setCode_type_poils(dbtp.ajouterTypePoils(description_type_poils));
     			if(newTypePoils.getCode_type_poils()==null) newTypePoils = null;
@@ -48,7 +48,7 @@ public class Poils {
       return message;	
     }
     
-    /* on verifie que le nom de poils ne contient pas de chiffres et de longueur entre 2 et 30 caractères */
+    /* on verifie que le nom de poils ne contient pas de chiffres et de longueur entre 2 et 30 caractÃ¨res */
     public boolean verifNomTypePoils(String description_type_poils) {
     		return description_type_poils.matches("[^0-9]{2,30}");
     }
