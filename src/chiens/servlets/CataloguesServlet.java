ca29bdf4b53c9d9725f0906d05aa24f83bc07e3a
MKazakova <45536947+MKazakova@users.noreply.github.com>
2018-12-08 20:27:11 +0100

    Update CataloguesServlet.java

--- src/chiens/servlets/CataloguesServlet.java
+++ src/chiens/servlets/CataloguesServlet.java
@@ -26,7 +26,7 @@ import chiens.metier.Catalogues;
  */
 @WebServlet("/CataloguesServlet")
 public class CataloguesServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -40,38 +40,39 @@ public class CataloguesServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 			
-	int doIt() throws Exception {  throw new Exception("FORGET IT");  }
+   int doIt() throws Exception {  throw new Exception("FORGET IT");  
+   }
 	
-	/*Utilisé pour affichage des differents tableau existants dans la base des données chiens*/
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	/*UtilisÃ© pour affichage des differents tableau existants dans la base des donnÃ©es chiens*/
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		String whatsend = request.getParameter("whatsend");
 		if(whatsend.equals("Voir la liste des couleurs")) {//pour affichage de tableau des couleurs
-			setAttrCouleur(request);//établissement des attributes de session correspondants
+			setAttrCouleur(request);//Ã©tablissement des attributes de session correspondants
 			ServletContext sc = request.getSession().getServletContext();    
 			RequestDispatcher rd = sc.getRequestDispatcher("/administrateur/listCouleurs.jsp"); 
 			rd.forward(request, response);
 		}
 		else if(whatsend.equals("Voir la liste aptitudes")) {//pour affichage de tableau des aptitudes
-			setAttrAptitude(request);//établissement des attributes de session correspondants
+			setAttrAptitude(request);//Ã©tablissement des attributes de session correspondants
 			ServletContext sc = request.getSession().getServletContext();    
 			RequestDispatcher rd = sc.getRequestDispatcher("/administrateur/listAptitudes.jsp");
 			rd.forward(request, response);
 		}
 		else if(whatsend.equals("Voir la liste de types de poils")) {//pour affichage de tableau de types de poils
-			setAttrTypePoils(request);//établissement des attributes de session correspondants
+			setAttrTypePoils(request);//Ã©tablissement des attributes de session correspondants
 			ServletContext sc = request.getSession().getServletContext();    
 			RequestDispatcher rd = sc.getRequestDispatcher("/administrateur/listTypePoils.jsp");
 			rd.forward(request, response);
 		}
 		else if(whatsend.equals("Voir la liste des pays")) {//pour affichage de tableau des pays
-			setAttrPays(request);//établissement des attributes de session correspondants
+			setAttrPays(request);//Ã©tablissement des attributes de session correspondants
 			ServletContext sc = request.getSession().getServletContext();    
 			RequestDispatcher rd = sc.getRequestDispatcher("/administrateur/listPays.jsp");
 			rd.forward(request, response);
 		}
-		else if(whatsend.equals("Voir la liste elevages")) {//affichage de tableau des élévages
-			setAttrElevage(request);//établissement des attributes de session correspondants
+		else if(whatsend.equals("Voir la liste elevages")) {//affichage de tableau des Ã©lÃ©vages
+			setAttrElevage(request);//Ã©tablissement des attributes de session correspondants
 			ServletContext sc = request.getSession().getServletContext();    
 			RequestDispatcher rd = sc.getRequestDispatcher("/administrateur/listElevages.jsp");
 			rd.forward(request, response);
@@ -81,87 +82,87 @@ public class CataloguesServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	/*gère la suppression des éléments des tableaux differents en fonction de la valeur de whatsend*/
+	/*gÃ¨re la suppression des Ã©lÃ©ments des tableaux differents en fonction de la valeur de whatsend*/
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		String whatsend = request.getParameter("whatsend");
 		
-		if(whatsend.equals("delete")) {//première demande de suppression de couleur
+		if(whatsend.equals("delete")) {//premiÃ¨re demande de suppression de couleur
 			String code = request.getParameter("element");
 			String nom = DBCouleurs.getNomParCode(Integer.parseInt(code));
-			setAttrCouleur(request);//établissement des attributes de session correspondants
+			setAttrCouleur(request);//Ã©tablissement des attributes de session correspondants
 			request.setAttribute("code", code);
 			request.setAttribute("nom", nom);
-			request.setAttribute("confirmation", true);//affiche une fenêtre de confirmation de suppression de couleur
+			request.setAttribute("confirmation", true);//affiche une fenÃªtre de confirmation de suppression de couleur
 			ServletContext sc = request.getSession().getServletContext();    
 			RequestDispatcher rd = sc.getRequestDispatcher("/administrateur/listCouleurs.jsp"); 
 			rd.forward(request, response);
 		}
-		else if(whatsend.equals("SuppressionConfirme")) {//la suppression est confirmé, on supprime la couleur
+		else if(whatsend.equals("SuppressionConfirme")) {//la suppression est confirmÃ©, on supprime la couleur
 			String code=request.getParameter("element");
 			DBCouleurChien.deleteCouleurParCodeCouleur(code);
 			DBCouleurs.deleteCouleur(code);
-			setAttrCouleur(request);//établissement des attributes de session correspondants
+			setAttrCouleur(request);//Ã©tablissement des attributes de session correspondants
 			ServletContext sc = request.getSession().getServletContext();    
 			RequestDispatcher rd = sc.getRequestDispatcher("/administrateur/listCouleurs.jsp"); 
 			rd.forward(request, response);
 		}
-		else if(whatsend.equals("SuppressionConfirmeApt")) {//la suppression d'une aptitude est confirmé, on la supprime
+		else if(whatsend.equals("SuppressionConfirmeApt")) {//la suppression d'une aptitude est confirmÃ©, on la supprime
 			String code=request.getParameter("element");
 			DBAptitudeChien.deleteAptitudeParCodeAptitude(code);
 			DBAptitude.deleteAptitude(code);
-			setAttrAptitude(request);//établissement des attributes de session correspondants
+			setAttrAptitude(request);//Ã©tablissement des attributes de session correspondants
 			ServletContext sc = request.getSession().getServletContext();    
 			RequestDispatcher rd = sc.getRequestDispatcher("/administrateur/listAptitudes.jsp"); 
 			rd.forward(request, response);
 		}
-		else if(whatsend.equals("deleteApt")) {//première demande de suppression d'une aptitude
+		else if(whatsend.equals("deleteApt")) {//premiÃ¨re demande de suppression d'une aptitude
 			String code = request.getParameter("element");
 			String nom = DBAptitude.getNomParCode(Integer.parseInt(code));
-			setAttrAptitude(request);//établissement des attributes de session correspondants
+			setAttrAptitude(request);//Ã©tablissement des attributes de session correspondants
 			request.setAttribute("code", code);
 			request.setAttribute("nom", nom);
-			request.setAttribute("confirmation", true);//affiche une fenêtre de confirmation de suppression de couleur
+			request.setAttribute("confirmation", true);//affiche une fenÃªtre de confirmation de suppression de couleur
 			ServletContext sc = request.getSession().getServletContext();    
 			RequestDispatcher rd = sc.getRequestDispatcher("/administrateur/listAptitudes.jsp"); 
 			rd.forward(request, response);
 		}
-		else if(whatsend.equals("SuppressionConfirmeTP")) {//la suppression d'un type de poils est confirmé, on le supprime
+		else if(whatsend.equals("SuppressionConfirmeTP")) {//la suppression d'un type de poils est confirmÃ©, on le supprime
 			String code=request.getParameter("element");
 			DBPoilsChien.deleteTypePoilsParCodeTypePoils(code);
 			DBTypePoils.deleteTypePoils(code);
-			setAttrTypePoils(request);//établissement des attributes de session correspondants
+			setAttrTypePoils(request);//Ã©tablissement des attributes de session correspondants
 			ServletContext sc = request.getSession().getServletContext();    
 			RequestDispatcher rd = sc.getRequestDispatcher("/administrateur/listTypePoils.jsp"); 
 			rd.forward(request, response);
 		}
-		else if(whatsend.equals("deleteTP")) {//la première demande de suppression de type de poils
+		else if(whatsend.equals("deleteTP")) {//la premiÃ¨re demande de suppression de type de poils
 			String code = request.getParameter("element");
 			String nom = DBTypePoils.getNomParCode(Integer.parseInt(code));
-			setAttrTypePoils(request);//établissement des attributes de session correspondants
+			setAttrTypePoils(request);//Ã©tablissement des attributes de session correspondants
 			request.setAttribute("code", code);
 			request.setAttribute("nom", nom);
-			request.setAttribute("confirmation", true);//affiche une fenêtre de confirmation de suppression de type de poils
+			request.setAttribute("confirmation", true);//affiche une fenÃªtre de confirmation de suppression de type de poils
 			ServletContext sc = request.getSession().getServletContext();    
 			RequestDispatcher rd = sc.getRequestDispatcher("/administrateur/listTypePoils.jsp"); 
 			rd.forward(request, response);
 		}
-		else if(whatsend.equals("deletePays")) {//la première demande de suppression de pays
+		else if(whatsend.equals("deletePays")) {//la premiÃ¨re demande de suppression de pays
 			String code = request.getParameter("element");
 			if(DBRaceChien.RacesOriginairesExistent(code)) request.setAttribute("liee", true);
 			String nom = DBPays.getNomDePays(Integer.parseInt(code));
 			request.setAttribute("code", code);
 			request.setAttribute("nom", nom);
-			request.setAttribute("confirmation", true);//affiche une fenêtre de confirmation de suppression de pays
-			setAttrPays(request);//établissement des attributes de session correspondants
+			request.setAttribute("confirmation", true);//affiche une fenÃªtre de confirmation de suppression de pays
+			setAttrPays(request);//Ã©tablissement des attributes de session correspondants
 			ServletContext sc = request.getSession().getServletContext();    
 			RequestDispatcher rd = sc.getRequestDispatcher("/administrateur/listPays.jsp");
 			rd.forward(request, response);
 		}
-		else if(whatsend.equals("SuppressionConfirmePays")) {//la suppression d'un pays est confirmé, on le supprime
+		else if(whatsend.equals("SuppressionConfirmePays")) {//la suppression d'un pays est confirmÃ©, on le supprime
 			String code=request.getParameter("element");
 			DBPays.deletePays(code);
-			setAttrPays(request);//établissement des attributes de session correspondants
+			setAttrPays(request);//Ã©tablissement des attributes de session correspondants
 			ServletContext sc = request.getSession().getServletContext();    
 			RequestDispatcher rd = sc.getRequestDispatcher("/administrateur/listPays.jsp"); 
 			rd.forward(request, response);
@@ -169,18 +170,18 @@ public class CataloguesServlet extends HttpServlet {
 		else if(whatsend.equals("deleteElevage")) {
 			String code = request.getParameter("element");
 			String nom = DBElevage.getNomParCode(Integer.parseInt(code));
-			setAttrElevage(request);//établissement des attributes de session correspondants
+			setAttrElevage(request);//Ã©tablissement des attributes de session correspondants
 			request.setAttribute("code", code);
 			request.setAttribute("nom", nom);
-			request.setAttribute("confirmation", true);//affiche une fenêtre de confirmation de suppression de pays
+			request.setAttribute("confirmation", true);//affiche une fenÃªtre de confirmation de suppression de pays
 			ServletContext sc = request.getSession().getServletContext();    
 			RequestDispatcher rd = sc.getRequestDispatcher("/administrateur/listElevages.jsp"); 
 			rd.forward(request, response);
 		}
-		else if(whatsend.equals("SuppressionConfirmeElevage")) {//la suppression d'un élévage est confirmé, on le supprime
+		else if(whatsend.equals("SuppressionConfirmeElevage")) {//la suppression d'un Ã©lÃ©vage est confirmÃ©, on le supprime
 			String code=request.getParameter("element");
 			Catalogues.deleteElevage(code);
-			setAttrElevage(request);//établissement des attributes de session correspondants
+			setAttrElevage(request);//Ã©tablissement des attributes de session correspondants
 			ServletContext sc = request.getSession().getServletContext();    
 			RequestDispatcher rd = sc.getRequestDispatcher("/administrateur/listElevages.jsp"); 
 			rd.forward(request, response);
@@ -188,8 +189,8 @@ public class CataloguesServlet extends HttpServlet {
 	}
 
 	/*Les methodes suivantes ajoute les attributes necessaires pour l'affichage de l'information 
-	 * sur la page dans le request passé en paramètre*/
-	private static void setAttrAptitude(HttpServletRequest request) {
+	 * sur la page dans le request passÃ© en paramÃ¨tre*/
+    private static void setAttrAptitude(HttpServletRequest request) {
     	request.setAttribute("races_aptitudes", Catalogues.getListRacesChienAptitudeDonne());
 		request.setAttribute("listatributes", Catalogues.getListAptitudes());
     }
