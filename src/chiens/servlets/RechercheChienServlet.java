5c920fee1f68dbb68c7e42a446394e762609a406
MKazakova <45536947+MKazakova@users.noreply.github.com>
2018-12-08 20:34:59 +0100

    Update RechercheChienServlet.java

--- src/chiens/servlets/RechercheChienServlet.java
+++ src/chiens/servlets/RechercheChienServlet.java
@@ -23,7 +23,7 @@ import chiens.metier.TestRace;
  */
 @WebServlet("/RechercheChienServlet")
 public class RechercheChienServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -35,9 +35,9 @@ public class RechercheChienServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String whatsend = request.getParameter("whatsend");
-		/*redirige l'utilisateur sur la page de recherche d'une race par paramètres à partir de 
+		/*redirige l'utilisateur sur la page de recherche d'une race par paramÃ¨tres Ã  partir de 
 		 * l'espace d'administrateur */
 		if(whatsend.equals("rechercheAdmin")) { 
 		    setAttrRaceAsLists(request);
@@ -45,7 +45,7 @@ public class RechercheChienServlet extends HttpServlet {
 		    RequestDispatcher rd = sc.getRequestDispatcher("/user/rechercheRace.jsp"); 
 		    rd.forward(request, response);
 		}
-		/*redirige l'utilisateur sur la page de recherche d'une race par paramètres à partir de 
+		/*redirige l'utilisateur sur la page de recherche d'une race par paramÃ¨tres Ã  partir de 
 		 * la page d'accueil */
 		else if(whatsend.equals("rechercheUser")) { 
 			setAttrRaceAsLists(request);
@@ -59,33 +59,33 @@ public class RechercheChienServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String whatsend = request.getParameter("whatsend");
-		/*le cas d'utilisateur qui a passé un test détérminant la race de chien qui lui correspond*/
+		/*le cas d'utilisateur qui a passÃ© un test dÃ©tÃ©rminant la race de chien qui lui correspond*/
 		if(whatsend.equals("test")) { 
-			/*on crée une variable de type TestRace*/
+			/*on crÃ©e une variable de type TestRace*/
 		    TestRace test = new TestRace(request);
-		    /*et essaye d'obtenir une liste de races de chiens correspondantes à la situation
+		    /*et essaye d'obtenir une liste de races de chiens correspondantes Ã  la situation
 		     *de l'utilisateur*/
 		    List<RaceChienBean> races = test.test();
 		    try {
-			  if(!test.isComplet()) throw new Exception();/*si un teste n'a pas été remplie correctement
-			  on jette une exception qui sera traité dans le block catch*/
-			  if(!races.isEmpty()) {/*si le test avait été traité correctement(donc on n'est pas sortie du try
+			  if(!test.isComplet()) throw new Exception();/*si un teste n'a pas Ã©tÃ© remplie correctement
+			  on jette une exception qui sera traitÃ© dans le block catch*/
+			  if(!races.isEmpty()) {/*si le test avait Ã©tÃ© traitÃ© correctement(donc on n'est pas sortie du try
 			  on verifie si la liste de races correspondantes n'est pas vide*/
-				request.setAttribute("trouve", true);//s'il n'est pas vide, on initialise trouve à true
-				request.setAttribute("races", races);//et crée une attribute de request avec ces races
+				request.setAttribute("trouve", true);//s'il n'est pas vide, on initialise trouve Ã  true
+				request.setAttribute("races", races);//et crÃ©e une attribute de request avec ces races
 				setAttrRaceAsMaps(request);
 			  }
-			  else request.setAttribute("notrouve", true);//si aucune race correspondante n'a été trouvé
+			  else request.setAttribute("notrouve", true);//si aucune race correspondante n'a Ã©tÃ© trouvÃ©
 
 			    ServletContext sc = request.getSession().getServletContext();
 			    RequestDispatcher rd = sc.getRequestDispatcher("/user/page_d_accueil.jsp"); 
 			    rd.forward(request, response);
 		      }
 		    catch (Exception e) {
-			  if(!test.getErreur().isEmpty()){//si le test n'a pas été remplie correctement
-				//on envoie une liste des messages d'erreurs à l'utilisateur
+			  if(!test.getErreur().isEmpty()){//si le test n'a pas Ã©tÃ© remplie correctement
+				//on envoie une liste des messages d'erreurs Ã  l'utilisateur
 				request.setAttribute("erreur", test.getErreur());
 			    request.setAttribute("echec", true);
 				ServletContext sc = request.getSession().getServletContext();
@@ -94,10 +94,10 @@ public class RechercheChienServlet extends HttpServlet {
 			  }
 		   }
 		}
-		else {//si la recherche de race avait été effectué à travers le formulaire de recherche directe(sans test)
-			/*on cherche des races correspondante à la recherche*/
+		else {//si la recherche de race avait Ã©tÃ© effectuÃ© Ã  travers le formulaire de recherche directe(sans test)
+			/*on cherche des races correspondante Ã  la recherche*/
 			ArrayList<RaceChienBean> races = RechercheRaceChien.getRacesCorrespondantes(request);
-			if(!races.isEmpty()) {//si les races correspondante avaient été crées
+			if(!races.isEmpty()) {//si les races correspondante avaient Ã©tÃ© crÃ©es
 				request.setAttribute("trouve", true);
 				request.setAttribute("races", races);
 				setAttrRaceAsMaps(request);
@@ -110,8 +110,8 @@ public class RechercheChienServlet extends HttpServlet {
 		  }
 	}
 	
-	/*une methodes qui instaurent les attributes necessaires à l'affichage*/
-	private static void setAttrRaceAsLists(HttpServletRequest request) {
+	/*une methodes qui instaurent les attributes necessaires Ã  l'affichage*/
+     private static void setAttrRaceAsLists(HttpServletRequest request) {
 		HttpSession session = request.getSession();
 		session.setAttribute("payslist", Catalogues.getListPays());
 		session.setAttribute("tplist", Catalogues.getListTypePoils());
@@ -119,7 +119,7 @@ public class RechercheChienServlet extends HttpServlet {
 		session.setAttribute("cclist", Catalogues.getListDeCouleurs());
 	}
 
-	private static void setAttrRaceAsMaps(HttpServletRequest request) {
+      private static void setAttrRaceAsMaps(HttpServletRequest request) {
 		request.setAttribute("pays", Catalogues.getMapDePays());
 		request.setAttribute("aptitudes", Catalogues.getMapAptitudes());
 		request.setAttribute("couleurs", Catalogues.getMapCouleursParCodeRace());
